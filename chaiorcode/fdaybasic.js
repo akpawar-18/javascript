90b8581e70862548944a8c21164f428401961360
akpawar-18 <119653526+akpawar-18@users.noreply.github.com>
2024-01-12 20:27:44 -0800

    fdaybasic.js

---
+++ chaiorcode/fdaybasic.js
@@ -0,0 +1,21 @@
+console.log("akshada");
+const accountId = 1234;
+let Accountemail = "akshada@gmail.com";
+var accountPassword = "123455";
+accountyCity = "nagar";
+//accountId = 2; //not allowed
+console.log(accountId);
+
+Accountemail = "pawar@gmail.com";
+accountPassword = "23434544";
+accountyCity = "rampur";
+let accountstate;
+
+//console.log(accountId);
+
+/*
+prefer not to use var
+becouse of issue in block scope and functional scope
+*/
+                                                    
+console.table([Accountemail, accountPassword, accountyCity, accountstate]);
