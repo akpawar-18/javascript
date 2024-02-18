4b610b74f99f593539583553770673fa9fce14a9
akpawar-18 <119653526+akpawar-18@users.noreply.github.com>
2024-02-17 20:48:01 -0800

    IIFE

---
+++ chaiorcode/iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expression (IIFE) 
+// IIFE is used in js because global scope ke polution ko hatane ke liye IIFE ko use karte hai.
+// global scope polution(variables, functions) OR immediate execution ke liye bhi IIFE ka use karte hai 
+// Syntax = (function def)()
+
+(function chai(){
+    // named IIFE
+    console.log(`DB Connected`);
+})();
+
+((name) => {
+    // simple IIFE
+console.log(`DB Connected Two ${name}`);
+})("akshyaa")
